ec2d1fdf56c0e7af7a3b12d19bf672d8a7ae37c8
PyEarL <99483173+PyEarL@users.noreply.github.com>
2022-07-31 16:37:32 +0300

    Add files via upload

---
+++ edu.py
@@ -0,0 +1,6 @@
+magicians = ['Alice', 'Tom', 'Kate']
+for magician in magicians:
+	print(magician.title() + ' is the best magician ever')
+	print("I can't wait " + magician + " make magic" + ".\n")
+print("Thank you everyone!")
+
---
+++ squares.py
@@ -0,0 +1,6 @@
+values=list(range(1,11))
+squares=[]
+for number in values:
+	number**=2
+	squares.append(number) 
+print (squares)
